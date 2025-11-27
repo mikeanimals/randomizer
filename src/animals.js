4216bb6bd847cf90d61cb42b599e2a38b639d34b
tokensm1x <tokensmix@gmail.com>
2025-11-27 12:07:54 +0300

    Update display status for an animal entry, setting it to false to refine visibility in the dataset.

--- src/animals.js
+++ src/animals.js
@@ -91,7 +91,7 @@ export const animals = [
     wishlist: [],
     closingMessage:
       "Santa, ich wünsche dir magische Stimmung, viele Lächeln und Freude an jedem Tag! 🎄",
-    display: true,
+    display: false,
   },
   {
     name: "Muta",
